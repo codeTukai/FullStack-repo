5c111ba4045da0c05f4b62cb362a182f9a456bda
codeTukai <tukaighosh753@gmail.com>
2026-04-15 10:17:18 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+import express from 'express'
+
+var cors = require('cors')
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+// app.get('/',(req,res)=>{
+//     res.send("Hii, Welcome to my site")
+// })
+
+app.get('/jokes', (req,res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "my first joke",
+            content: "Where do most horses live? In neighhh-borhoods!"
+        },
+        {
+            id: 2,
+            title: "my second joke",
+            content: " Why did the Daddy Rabbit go to the barber? He had a lot of little hares"
+        },
+        {
+            id: 3,
+            title: "my third joke",
+            content: " How do you make a lemon drop? Let it fall from the tree."
+        },
+        {
+            id: 1,
+            title: "my fourth joke",
+            content: "Why can’t you ever trust atoms? Because they make up everything."
+        },
+        {
+            id: 5,
+            title: "my fifth joke",
+            content: "which planet loves to sing? Nep-tune"
+        },
+        {
+            id: 6,
+            title: "my last joke",
+            content: " What’s a private investigator’s favorite shoe? Sneak-ers"
+        },
+        
+    ];
+    res.send(jokes);
+
+})
+
+app.listen(port, ()=>{
+    console.log(`port running on http://localhost:${port}`);
+    
+})
+
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+function App() {
+ const [jokes, setJokes] = useState([])
+
+ useEffect(()=>{
+  axios.get('http://localhost:3000/jokes')
+  .then((response)=>{
+    setJokes(response.data)
+    // console.log(response);
+    
+  })
+  .catch((error)=>{
+    console.log(error);
+    
+  })
+
+ },[])
+ 
+
+  return (
+   <>
+   <h1>welcome to my site</h1>
+   <p>JOKES: {jokes.length}</p>
+   {
+    jokes.map((joke, index)=>{
+      <div key={joke.id}>
+        <h1>{joke.title}</h1>
+        <p>{joke.content}</p>
+      </div>
+    })
+
+   }
+   </>
+  )
+}
+
+export default App
